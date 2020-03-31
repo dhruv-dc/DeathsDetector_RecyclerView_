15cb4882f89710f04fe419935bacfb3e2c0b4684
dhruv-dc <53862511+dhruv-dc@users.noreply.github.com>
2020-03-31 17:06:14 +0530

    Modified MainActivity Added new Cardview Showing Error on TextView

--- app/src/main/java/com/example/deathsdetector/MainActivity.java
+++ app/src/main/java/com/example/deathsdetector/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -24,20 +26,60 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public TextView stateTextview;
+    public TextView positiveCasesTextview;
+    public TextView deathsInStateTextview;
+    public TextView dischargedTextview;
+    public TextView stateCounterTextview;
+    public ImageButton nextButton;
+    public ImageButton prevButton;
+    private int currentStateIndex = 0;
+    private int currentPositiveCasesIndex=0;
+    private int currentDischargedIndex=0;
+    private int currentDeathInStateIndex=0;
+    public List<Question> questionList;
     //public String url = "https://api.rootnet.in/covid19-in/stats/latest";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<Question> questionList = new QuestionBank().getQuestion(new AnswerAsyncResponse() {
+
+        prevButton = findViewById(R.id.previous_button);
+        nextButton = findViewById(R.id.next_button);
+
+        dischargedTextview = findViewById(R.id.discharged_text_view);
+        deathsInStateTextview = findViewById(R.id.deaths_sw_text_view);
+        positiveCasesTextview = findViewById(R.id.pc_text_view);
+        stateCounterTextview = findViewById(R.id.counter_text);
+        stateTextview = findViewById(R.id.state_text_view);
+
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+
+            questionList = new QuestionBank().getQuestion(new AnswerAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
-                Log.d("Inside", "processFinished: "+questionArrayList);
+
+//                stateTextview.setText(questionArrayList.get(currentStateIndex).getStateNames());
+//                dischargedTextview.setText(questionArrayList.get(currentDischargedIndex).getDischarged());
+//                deathsInStateTextview.setText(questionArrayList.get(currentDeathInStateIndex).getDeathsInState());
+//                positiveCasesTextview.setText(questionArrayList.get(currentPositiveCasesIndex).getPositiveCasesInState());
+
+                Log.d("statename ; ", "processFinished: "+questionArrayList.get(currentStateIndex).getStateNames());
+                Log.d("positivecases ", "processFinished: "+questionArrayList.get(currentPositiveCasesIndex).getPositiveCasesInState());
             }
         });
         //Log.d("Main", "onCreate: "+questionList);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.previous_button:
+            case R.id.next_button:
+                break;
+        }
+    }
 }
